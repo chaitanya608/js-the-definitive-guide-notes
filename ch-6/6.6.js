fc245ebe73912e82dc807413b5af1f2a6b4e427c
chaitanya-qapita <chaitanya.vuddanti@qapitacorp.com>
2021-10-10 21:49:49 +0530

    demo: for/in loop enumerates both own and inherited enumerable props of an object

---
+++ ch-6/6.6.js
@@ -0,0 +1,21 @@
+/*
+To demonstrate that for/in loop enumerates both own and inherited enumerable properties of an object
+*/
+
+const obj1 = {
+  key11: "value11",
+  key12: "value12",
+  key13: "value13",
+};
+
+const obj2 = {
+  key21: "value21",
+  key22: "value22",
+  key23: "value23",
+};
+obj2.prototype = obj1;
+
+for (let prop in obj2) {
+  // console.log(obj2[prop]);
+  console.log(prop);
+}
