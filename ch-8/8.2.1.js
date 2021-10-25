22c2875e0eabae5aeb8665fe0466819254d1010c
chaitanya-qapita <chaitanya.vuddanti@qapitacorp.com>
2021-10-25 21:10:56 +0530

    demo: a fn (unless arrow fn) does not inherit execution context of its parent fn

--- ch-8/8.2.1.js
+++ ch-8/8.2.1.js
@@ -4,10 +4,25 @@ console.log("");
 
 const obj1 = {
   fn1: function () {
-    console.log("this in fn1: ", this); // this refers to obj1, both in strict and non-strict mode.
+    console.log("this in fn1: ", this); // this refers to obj1, both in strict and non-strict modes.
+
+    function nestedFn1() {
+      console.log("this in nestedFn1: ", this); // 'this' is undefined in strict mode and refers to Window object in non-strict mode.
+    }
+    nestedFn1();
+
+    const arrowFnInFn1 = () => {
+      console.log("this in arrowFnInFn1: ", this); // 'this' refers to obj1, bth in strict and non-strict modes.
+    };
+    arrowFnInFn1();
   },
   fn2: () => {
     console.log("this in fn2: ", this); // this refers to Window object, both in strict and non-strict mode.
+
+    const nestedFn2 = () => {
+      console.log("this in nestedFn2: ", this); // 'this' refers to Window object in strict mode and non-strict mode.
+    };
+    nestedFn2();
   },
 };
 
@@ -16,6 +31,7 @@ obj1.fn2();
 
 //////////////////////////////////////////////////////////////
 
+console.log("********************************************");
 function fn1() {
   console.log("this in fn1: ", this); // 'this' is undefined in strict mode and refers to Window in non-strict mode.
 }
